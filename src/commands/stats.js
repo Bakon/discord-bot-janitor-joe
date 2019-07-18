491128783f4145b691ae27fc5c217bfde4dc46a0
Julio Schilders <julio.schilders@gmail.com>
2019-07-19 00:52:55 +0200

    Added border around displayed stats

---
+++ src/commands/stats.js
@@ -0,0 +1,55 @@
+import {getPlayerStats} from '../getplayerstats';
+
+export const config = {
+  name: 'compare',
+  aliases: ['getstats', 'stats'],
+};
+
+export const run = async (bot, message, args) => {
+  getPlayerStats(args[0]).then(result => {
+    message.channel.send('** ** \n' + result.stats.join('\n'));
+
+    if (args[1]) {
+      const player1Stats = [];
+      const player2Stats = [];
+      const results = () => {};
+
+      getPlayerStats(args[0]).then(result => {
+        player1Stats.push(result);
+      });
+
+      getPlayerStats(args[1]).then(result => {
+        player2Stats.push(result);
+      });
+
+      Promise.all([getPlayerStats([0]), getPlayerStats([1])]).then(
+        message.channel.send('** ** \n' + results.join('\n'))
+      );
+    }
+    // Promise.all([fetchData(args[0]), fetchData(args[1])]).then(console.log);
+  });
+};
+
+// Iron 4 = 0
+// Iron 3 = 100
+// Iron 2 = 200
+// Iron 1 = 300
+// Silver 4 = 300
+// Silver 3 = 400
+// Silver 2 = 500
+// Silver 1 = 600
+// Gold 4 = 600
+// Gold 3 = 700
+// Gold 2 =  800
+// Gold I = 900
+// Plat 4 = 900
+// Plat 3 = 1000
+// Plat 2 = 1100
+// Plat 1 = 1200
+// Dia 4 = 1200
+// Dia 3 = 1300
+// Dia 2 = 1400
+// Dia 1 = 1500
+// Master = 1600
+// Grandmaster = 1700
+// Challenger = 1800
--- src/getplayerstats.js
+++ src/getplayerstats.js
@@ -1,37 +1,62 @@
 import fetch from 'node-fetch';
 import {parse} from 'node-html-parser';
 
-export const getPlayerStats = username => {
-  return fetch(`https://euw.op.gg/summoner/userName=${username}`)
+export const getPlayerStats = searchQuery => {
+  return fetch(`https://euw.op.gg/summoner/userName=${searchQuery}`)
     .then(res => res.text())
     .then(html => {
       const parsedHTML = parse(html);
+      const username = parsedHTML.querySelector('.Profile .Name').rawText;
       const pastRankList = parsedHTML
         .querySelector('.PastRankList')
         .childNodes.toString();
 
-      const currentSeason =
-        'Current: ' +
-        parsedHTML.querySelector('.TierRank').innerHTML +
-        ' ' +
-        parsedHTML.querySelector('.LeaguePoints').innerHTML.trim();
-
-      let previousSeasonsPlayed = parsedHTML
+      let allSeasonsPlayed = parsedHTML
         .querySelectorAll('.PastRankList li')
         .map(li => {
           return [
             'Season ' + li.querySelector('b').rawText.substr(1, 1),
-            li.attributes.title === undefined
-              ? li.rawText.trim().substr(3, 4)
-              : li.attributes.title,
-          ].join(': ');
+            li.attributes.title || li.rawText.trim().slice(3),
+          ];
         });
 
-      previousSeasonsPlayed.reverse();
+      const currentStats =
+        parsedHTML.querySelector('.TierRank').innerHTML +
+        ' ' +
+        parsedHTML.querySelector('.LeaguePoints').innerHTML.trim();
+
+      allSeasonsPlayed.push(['Current', currentStats]);
+
+      const firstColumnLengths = allSeasonsPlayed.map(s => s[0].length);
+      const lastColumnLengths = allSeasonsPlayed.map(s => s[1].length);
+      const firstColumnMax = Math.max(...firstColumnLengths);
+      const lastColumnMax = Math.max(...lastColumnLengths);
+
+      const stats = allSeasonsPlayed.map(season => {
+        return [
+          season[0].padEnd(firstColumnMax, ' '),
+          season[1].padEnd(lastColumnMax, ' '),
+        ];
+      });
+
+      stats.reverse();
+
+      const finalStats = stats.map(columns => {
+        return `\`| ${columns[0]} | ${columns[1]} |\``;
+      });
+
+      // - 6 because white space & backticks (``)
+      const userLength = username.length + 1;
+      const maxLength = finalStats[0].length - 6;
+
+      finalStats.unshift(
+        `\`+ ${username} ${'-'.repeat(maxLength - userLength)} +\``
+      );
+      finalStats.push(`\`+ ${'-'.repeat(maxLength)} +\``);
 
       return {
         username,
-        stats: [currentSeason, ...previousSeasonsPlayed],
+        stats: finalStats,
       };
     });
 };
