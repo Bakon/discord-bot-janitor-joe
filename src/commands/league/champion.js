382c26dd878c5ecc2f669904914eee33cf6c81fb
Julio Schilders <julio.schilders@gmail.com>
2019-07-23 00:35:12 +0200

    Changed coinflip, made it so that users can add their own champion pool to choose from

--- src/commands/league/champion.js
+++ src/commands/league/champion.js
@@ -1,45 +1,83 @@
 import {Champs} from '../../data/champions';
+import {Users} from '../../data/prefferedchamps';
 
 export default class Champion {
   static name = 'champion';
-  static aliases = ['champ'];
+  static aliases = ['champ', 'champs', 'champions'];
   static description =
     'Returns a random champion, if given a lane it will return a champ for that lane\nFor example: /champion supp';
 
   static run(bot, message, args) {
     const suppliedRole = args[0];
-    const checkArgs = args[1] === 'all' || args[1] === 'list';
-    const rAdc = Champs.adc[Math.floor(Math.random() * Champs.adc.length)];
-    const rSupp = Champs.supp[Math.floor(Math.random() * Champs.supp.length)];
-    const rMid = Champs.mid[Math.floor(Math.random() * Champs.mid.length)];
-    const rJgl =
-      Champs.jungle[Math.floor(Math.random() * Champs.jungle.length)];
-    const rTop = Champs.top[Math.floor(Math.random() * Champs.top.length)];
+    const username = args[1].toLowerCase();
 
-    const randomChampion = () => {
-      switch (suppliedRole) {
-        case 'adc':
-        case 'bot':
-          return checkArgs ? ['\u200b', ...Champs.adc] : rAdc;
+    if (username) {
+      if (username in Users) {
+        const randomPool = function() {
+          switch (suppliedRole) {
+            case 'bot':
+            case 'adc':
+              return Users[username].adc[
+                Math.floor(Math.random() * Users[username].adc.length)
+              ];
 
-        case 'support':
-        case 'supp':
-          return checkArgs ? ['\u200b', ...Champs.supp] : rSupp;
+            case 'support':
+            case 'supp':
+              return Users[username].supp[
+                Math.floor(Math.random() * Users[username].supp.length)
+              ];
 
-        case 'mid':
-          return checkArgs ? ['\u200b', ...Champs.mid] : rMid;
+            case 'mid':
+              return Users[username].mid[
+                Math.floor(Math.random() * Users[username].mid.length)
+              ];
 
-        case 'jungle':
-        case 'jgl':
-          return checkArgs ? ['\u200b', ...Champs.jungle] : rJgl;
+            case 'jgl':
+            case 'jungle':
+              return Users[username].jungle[
+                Math.floor(Math.random() * Users[username].jungle.length)
+              ];
 
-        case 'top':
-          return checkArgs ? ['\u200b', ...Champs.supp] : rTop;
-
-        default:
-          return Champs.any[Math.floor(Math.random() * Champs.any.length)];
+            case 'top':
+              return Users[username].top[
+                Math.floor(Math.random() * Users[username].top.length)
+              ];
+          }
+        };
+        message.channel.send(randomPool());
+      } else {
+        message.chanell.send(
+          "I couldn't find a champion pool with this username..."
+        );
       }
-    };
-    message.channel.send(randomChampion());
+    } else {
+      const randomChampion = function() {
+        switch (suppliedRole) {
+          case 'bot':
+          case 'adc':
+            return Champs.adc[Math.floor(Math.random() * Champs.adc.length)];
+
+          case 'supp':
+          case 'support':
+            return Champs.supp[Math.floor(Math.random() * Champs.supp.length)];
+
+          case 'mid':
+            return Champs.mid[Math.floor(Math.random() * Champs.mid.length)];
+
+          case 'jgl':
+          case 'jungle':
+            return Champs.jungle[
+              Math.floor(Math.random() * Champs.jungle.length)
+            ];
+
+          case 'top':
+            return Champs.top[Math.floor(Math.random() * Champs.top.length)];
+
+          default:
+            return Champs.any[Math.floor(Math.random() * Champs.any.length)];
+        }
+      };
+      message.channel.send(randomChampion());
+    }
   }
 }
---
+++ src/commands/league/pool.js
@@ -0,0 +1,9 @@
+export default class Pool {
+  static name = 'pool';
+  static aliases = [];
+  static description = "Let's the user set his own champion pool";
+
+  static run(bot, message, args, options) {
+    message.channel.send('xd');
+  }
+}
